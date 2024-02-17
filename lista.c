61dcb1078c3f6f017ebfdc7a50976e8f7278b1f5
Almeida0106 <60981484+Almeida0106@users.noreply.github.com>
2024-02-17 00:22:47 +0000

    j01 a j09

--- lista.c
+++ lista.c
@@ -20,7 +20,7 @@ void j03 () {
  printf ("Resultado: %d\n", 30*27);   
 }
 //3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
-void G03 (){
+void j04 (){
  int num1 = 5; 
  int num2 = 8; 
  int num3 = 12;
@@ -29,19 +29,54 @@ void G03 (){
 }
 
 //4. Faça um programa que leia e imprima um número inteiro.
-
+void j05 (){
+  int numero;  
+  printf ("Informe um valor inteiro: \n");
+  scanf ("%d", &numero);  
+  printf("O valor informado foi: %d\n", numero);
+}
 
 //5. Faça um programa que leia dois números reais e os imprima.
-
+void j06(){
+  float num1, num2;
+  printf ("Primeiro numero:\n" );
+  scanf ("%f", &num1);
+  printf ("Segundo numero:\n" );
+  scanf ("%f", &num2);
+  printf ("Os valores digitados são: %.2f %.2f \n", num1, num2);  
+}
 //6. Faça um programa que leia um número inteiro e imprima o seu
 //   antecessor e o seu sucessor.
-
+void j07 (){
+ int valor, ant, suc;
+ printf ("Informe um valor inteiro \n");
+ scanf ("%d", &valor);
+ ant = valor -1;
+ suc = valor +1;
+ printf ("Antecessor: %d Sucessor %d", ant, suc);  
+}
 //7. Faça um programa que leia o nome o endereço e o telefone de
 //   um cliente e ao final, imprima esses dados.
+void j08 (){
+ char nome[50], endereco[70], telefone[14];   
+ printf ("Nome:\n");
+ scanf (" %50[^\n]",nome);
+ printf ("Endereço:\n");
+ scanf (" %70[^\n]",endereco);
+ printf ("Telefone:\n");
+ scanf (" %14[^\n]",telefone);  
+ printf ("Nome: %s\n Endereço: %s\n Telefone: %s\n", nome, endereco, telefone); 
+}
 
 //8. Faça um programa que leia dois números inteiros e imprima a
 //   subtração deles.
-
+void j09(){
+  int num1, num2;
+  printf ("Informe dois valores inteiros: \n");
+  scanf ("%d %d", &num1, &num2);
+  printf("A subtração dos valores: %d %d é: %d\n", num1, num2, num1-num2);
+  
+}
 //9. Faça um programa que leia umnúmero real e imprima ¼ deste número.
 
 //10. Faça um programa que leia três números reais e calcule a
@@ -78,7 +113,7 @@ void G03 (){
 //    imprimir o resultado da conversão.
 //    F = (9 x C +160) / 5
 
-//18. Faça um programa que calcule a quantidade de litros de combustível
+//18. Faça um programa que calcule a quantidade de litros de combustível  
 //    consumidos em uma viagem, sabendo-se que o carro tem autonomia de
 //    12 km por litro de combustível. O programa deverá ler o tempo
 //    decorrido na viagem e a velocidade média e aplicar as fórmulas:
@@ -97,14 +132,13 @@ void G03 (){
 //    programa deve imprimir o valor da prestação atrasada, o período
 //    de atraso, os juros que serão cobrados pelo período de atraso, o
 //    valor da prestação acrescido dos juros. Considere juros simples.
-
+  
 //20. Faça um programa que efetue a apresentação do valor da conversão
 //    em real (R$) de um valor lido em dólar (US$). Para isso, será
 //    necessário também ler o valor da cotação do dólar.
 
 int main (){
-   j02 ();
+ j09 ();
 
 return EXIT_SUCCESS;
-
-} 
+}
